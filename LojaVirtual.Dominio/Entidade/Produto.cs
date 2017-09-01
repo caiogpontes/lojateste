aa976bbfbcc5ce7d4f1f999df3cb1802f7aa43b2
Caio Pontes <caio@caiopontes.com.br>
2017-09-01 03:22:28 -0300

    Aula 6

--- LojaVirtual.Dominio/Entidade/Produto.cs
+++ LojaVirtual.Dominio/Entidade/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,9 @@ namespace LojaVirtual.Dominio.Entidade
     public class Produto
     {
         public int ProdutoId { get; set; }
-        public string  Nome { get; set; }
-        public string  Descricao { get; set; }
-        public decimal Preco { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
         public string Categoria { get; set; }
+        public decimal Preco { get; set; }
     }
 }
