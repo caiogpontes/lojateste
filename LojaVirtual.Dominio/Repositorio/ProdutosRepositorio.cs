39757dd4714bdecb0455cba6473a06a379c9983c
Caio Pontes <caio@caiopontes.com.br>
2017-09-03 15:33:09 -0300

    Aula 32

--- LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
+++ LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -1,6 +1,7 @@
 ﻿using LojaVirtual.Dominio.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,34 @@ namespace LojaVirtual.Dominio.Repositorio
         public IEnumerable<Produto> Produtos
         {
             get { return _context.Produtos; }
+        }
+
+        public void Salvar(Produto produto)
+        {
+            if (produto.ProdutoId == 0)
+            {
+                //Salvado
+                _context.Produtos.Add(produto);
+            }
+            else
+            {
+                _context.Entry(produto).State = EntityState.Modified;
+
+
+                //Alteração
+                //Produto prod = _context.Produtos.Find(produto.ProdutoId);
+
+                //if (prod != null)
+                //{
+                //    prod.Nome = produto.Nome;
+                //    prod.Descricao = produto.Descricao;
+                //    prod.Preco = produto.Preco;
+                //    prod.Categoria = produto.Categoria;
+                //}
+
+            }
+
+            _context.SaveChanges();
         }
     }
 }
