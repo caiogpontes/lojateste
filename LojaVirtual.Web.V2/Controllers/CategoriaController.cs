477513183f1ccb5199bb9f5a44a31c5ac8aa1996
Caio Pontes <caio@caiopontes.com.br>
2017-09-04 15:01:55 -0300

    Aula 55
    Add Web.V2

---
+++ LojaVirtual.Web.V2/Controllers/CategoriaController.cs
@@ -0,0 +1,29 @@
+﻿using LojaVirtual.Dominio.Repositorio;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace LojaVirtual.Web.V2.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private CategoriaRepositorio _repositorio;
+        
+        [OutputCache(Duration = 3600, Location = OutputCacheLocation.Server, VaryByParam = "none")]
+        public JsonResult ObterEsportes()
+        {
+            _repositorio = new CategoriaRepositorio();
+            var cat = _repositorio.ObterCategorias();
+
+            var categoria = from c in cat
+                            select new
+                            {
+                                c.CategoriaDescricao,
+                                CategoriaDescricaoSeo = c.CategoriaDescricao.ToSeoUrl(),
+                                c.CategoriaCodigo
+                            };
+            
+            return Json(categoria, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
